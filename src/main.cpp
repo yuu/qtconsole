eb188943a69258bf3e43986f04cc69b2d1dcfbee
yuu <yuu3@users.noreply.github.com>
2016-09-14 11:14:32 +0900

    See behavior for QColor

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include <QtCore/QCoreApplication>
+#include <QtCore/QDebug>
+#include <QtGui/QColor>
 
 int main(int argc, char **argv)
 {
     QCoreApplication app(argc, argv);
+    qDebug() << "colorF: " <<  QColor("#808080FF").alphaF();
 
     return app.exec();
 }
