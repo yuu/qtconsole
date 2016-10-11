3047c2de196f46ba67b7a21c4c69b784bc591809
yuu <yuu3@users.noreply.github.com>
2016-10-11 17:27:13 +0900

    QSettings examples. Need to fix path

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
 #include <QtCore/QCoreApplication>
+#include <QSettings>
+#include <QDebug>
 
 int main(int argc, char **argv)
 {
     QCoreApplication app(argc, argv);
+    QSettings s("~/.config/weston.ini", QSettings::IniFormat);
+    qDebug() << s.childGroups();
+    s.beginGroup("ivi-launcher");
+    qDebug() << s.childKeys();
+    qDebug() << s.value("path");
 
     return app.exec();
 }
