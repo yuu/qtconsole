84e66d1f603fe97dcd2dc328af5e2397bf3fc316
yuu <yuu@users.noreply.github.com>
2016-10-12 18:14:12 +0900

    Set environment variable in Qt

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include <QtCore/QCoreApplication>
+#include <QtGlobal>
+#include <QDebug>
 
 int main(int argc, char **argv)
 {
     QCoreApplication app(argc, argv);
+    const char *key = "AAAA";
+    qputenv(key, QByteArray("1"));
+    qDebug() << "key:" << qgetenv(key);
 
     return app.exec();
 }
