4bbfcdeb7bbb225c4f8d8a4136c021ed8c99c843
yuu <yuu@users.noreply.github.com>
2016-11-18 23:55:27 +0900

    Sample qt cmd line parser
    $ qtconsole --help
    Usage: ./qtconsole.app/Contents/MacOS/qtconsole [options] file
    qt template project
    
    Options:
    -h, --help     Displays this help.
    -v, --version  Displays version information.
    -f, --force    Overwrite existing files.
    
    Arguments:
    file           Change QML property via ini
    
    ---
    
    $ qtconsole filename.ini
    "filename.ini"

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,30 @@
 #include <QtCore/QCoreApplication>
 
+#include <QCommandLineParser>
+#include <QDebug>
+
 int main(int argc, char **argv)
 {
     QCoreApplication app(argc, argv);
+
+    QCommandLineParser parser;
+    parser.setApplicationDescription("qt template project");
+    parser.addHelpOption();
+    parser.addVersionOption();
+
+    parser.addPositionalArgument("file", QCoreApplication::translate("main", "Change QML property via ini"));
+
+    QCommandLineOption forceOption(QStringList() << "f" << "force",
+        QCoreApplication::translate("main", "Overwrite existing files."));
+    parser.addOption(forceOption);
+
+    parser.process(app);
+
+    const QStringList args = parser.positionalArguments();
+    for (auto &&x : args)
+        qDebug() << x;
+
+    qDebug() <<  parser.isSet(forceOption);
 
     return app.exec();
 }
